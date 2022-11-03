a523ebcc29e047d44abcbc9c7580a3e3097e1f76
gballardo23 <gabriella.ballardo@duke.edu>
2022-11-03 15:45:44 -0400

    Added warning/error messages to Task4

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,3 +2,4 @@ ENV 859 - Geospatial Data Analytics
 Problem Set 4 - ArcPy 
 
 Fall 2022
+Gabi Ballardo, gabriella.ballardo@duke.edu
--- Scripts/Task4.py
+++ Scripts/Task4.py
@@ -2,3 +2,24 @@
 # Gabi Ballardo
 # ENV 859 Fall 2022
 
+# Imports system modules
+import arcpy
+
+# Creates a user-input dataset variable
+Dataset = arcpy.GetParameterAsText(0)
+
+# Creates a describe object for a user-specified dataset
+dsc = arcpy.da.Describe(Dataset)
+
+# Indicates the dataset’s catalogPath property
+print(dsc['catalogPath'])
+
+#Message results back to the status window when run
+if dsc == "FeatureSet":
+    print(arcpy.AddWarning("The shape type is {}.".format(Dataset)))
+elif dsc == "RasterDataset":
+    print(arcpy.AddWarning("The format is {}.".format(Dataset)))
+else: 
+    print(arcpy.AddError("The dataset type is {}.".format(Dataset)))
+
+
